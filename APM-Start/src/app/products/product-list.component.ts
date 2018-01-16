702e01082b06344dbf89df53193da929b82f8536
Justus <mj.kyalo@gmail.com>
2018-01-16 09:09:01 +0300

    prod list component

---
+++ APM-Start/src/app/products/product-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+@Component({
+	selector: 'pm-product-list',
+	templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+  productListTitle: string = 'Product List'
+}
