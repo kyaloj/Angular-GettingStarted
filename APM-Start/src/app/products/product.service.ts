18da2329ebee9ec7d2bc1509f5cbcc39deb6de62
Justus <mj.kyalo@gmail.com>
2018-01-24 22:55:50 +0300

    exception handling

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -1,13 +1,23 @@
-import { IProduct } from './product'
-import { Injectable } from '@angular/core'
-import { HttpClient } from '@angular/common/http'
-import { Observable } from 'rxjs/Observable'
+import { IProduct } from './product';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
 
 @Injectable()
 export class ProductService {
 	private _productsUrl = '.api/products/products.json'
 	constructor(private _http: HttpClient){}
     getProducts(): Observable<IProduct[]> {
-       return this._http.get<IProduct[]>(this._productsUrl);
-    }
+	   return this._http.get<IProduct[]>(this._productsUrl)
+		.do(data => console.log(JSON.stringify(data)))
+		.catch(this.handleError);
+	}
+	
+	private handleError(err: HttpErrorResponse) {
+	  console.log(err.message);
+	  return Observable.throw(err.message);
+	}
 }
